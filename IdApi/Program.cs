cdc28d44275136f13ea9f5711efe818386edb53d
Devin Miller <devinmiller@codeonthebrain.com>
2019-04-07 16:31:00 -0700

    Added Trace sink to Serilog.

--- IdApi/Program.cs
+++ IdApi/Program.cs
@@ -23,6 +23,7 @@ namespace IdApi
                 .MinimumLevel.Override("System", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
                 .Enrich.FromLogContext()
+                .WriteTo.Trace(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
                 .CreateLogger();
 
